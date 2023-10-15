c679909d35e8dcf523bfce1f5ce54c74f241d023
Dexter Stephens <dexterfstephens@gmail.com>
2023-10-14 22:16:42 -0600

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Dexter Stephens on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
